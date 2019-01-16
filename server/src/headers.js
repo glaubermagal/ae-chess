c62c321f4d5e9d9a7d1f79fefee6438aeea53dc0
Glauber Magalhães <glauber.tecinfo@gmail.com>
2019-01-16 21:47:21 -0200

    Added CORS support and formatted the error response from Server.

---
+++ server/src/headers.js
@@ -0,0 +1,8 @@
+const headers = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
+    next();
+};
+
+module.exports = headers;
--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,27 @@
 import express from 'express'
-const app = new express()
 import allPossibleMoves from './all_possible_moves'
-const numberTurns = 2
+import headers from './headers'
+const app = new express();
+const numberTurns = 2;
 
+app.use(headers);
 
 app.get('/', (req, res) => {
-    const possibleMovesTurn1 = allPossibleMoves[ req.query.pos ]
+    const possibleMovesTurn1 = allPossibleMoves[ req.query.pos ];
     let possibleMovesNextTurns = [
         possibleMovesTurn1
-    ]
+    ];
 
     if ( possibleMovesTurn1 === undefined ) {
-        res.status(400).send('Invalid or undefined chess coordinate')
+        res.status(400).send({
+            'message': 'Invalid or undefined chess coordinate'
+        });
         return
     }
 
     for ( let turn = 1; turn < numberTurns; turn++ ) {
-        let possibleMovesNextTurn = []
-        const lastTurn = possibleMovesNextTurns[ possibleMovesNextTurns.length - 1 ]
+        let possibleMovesNextTurn = [];
+        const lastTurn = possibleMovesNextTurns[ possibleMovesNextTurns.length - 1 ];
 
         for ( let coordinate of lastTurn ) {
             possibleMovesNextTurn.push( ...allPossibleMoves[ coordinate ] )
